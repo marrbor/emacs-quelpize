650bcb8736e3bdb0c3372e40a75ca87160789a8c
Masahide Matsumoto <matsumoto.masahide@gmail.com>
2014-03-03 18:09:43 +0900

    Add .gradle to groovy mode list.

--- init.el
+++ init.el
@@ -316,6 +316,7 @@
 ;;;groovy
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
+(add-to-list 'auto-mode-alist '("\.gradle$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("\.gradle$" . groovy-mode))
 
