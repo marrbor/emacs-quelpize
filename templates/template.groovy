50fd31aa9950a5db9f1765be9fbb6b084a28c40e
Masahide Matsumoto <matsumoto.masahide@genetec.co.jp>
2014-05-09 15:11:07 +0900

    Add BusMod template.

--- templates/template.groovy
+++ templates/template.groovy
@@ -2,3 +2,23 @@
  *   Copyright (c) Genetec Corporation. All Rights Reserved.
  */
 
+package jp.survei.
+
+import net.iperfecta.Ruisdael.Base.BusMod
+
+import org.vertx.java.core.Future
+
+class CLASSNAME extends BusMod {
+
+  def spec = [:]
+
+  @Override def start(Future<Void> startedResult) {
+    super.start()
+    def confresult = chkconfig(config, spec)   // verify configuration.
+    logger.debug "chkconfig returns ${ confresult}."
+    if (confresult) startedResult.setFailure(confresult) // something wrong.
+    else { 
+      startedResult.setResult(null)
+    }
+  }
+}
