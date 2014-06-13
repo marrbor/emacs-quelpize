8052d259ebb23215fde206cccfce966da08983c7
Masahide Matsumoto <matsumoto.masahide@gmail.com>
2014-06-14 01:33:19 +0900

    Update groovy template.

--- templates/template.groovy
+++ templates/template.groovy
@@ -12,13 +12,17 @@ class %file-without-ext% extends BusMod {
 
   def spec = [:]
 
-  @Override def start(Future<Void> startedResult) {
+  @Override def start(Future<Void> sr) {
     super.start()
     def confresult = chkconfig(config, spec)   // verify configuration.
     logger.debug "chkconfig returns ${ confresult}."
-    if (confresult) startedResult.setFailure(confresult) // something wrong.
-    else { 
-      startedResult.setResult(null)
+    if (confresult) sr.setFailure(confresult) // something wrong.
+    else {
+      sr.setResult(null)
     }
+  }
+
+  @Override def stop() {
+    logger.info "Stopped."
   }
 }
